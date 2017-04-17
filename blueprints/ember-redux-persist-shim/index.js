9072eb5c1ec367aed1ec463f531905393df3df23
toran billups <toranb@gmail.com>
2017-04-17 18:34:13 -0500

    added lodash-es/ and stringify shims to blueprint

--- blueprints/ember-redux-persist-shim/index.js
+++ blueprints/ember-redux-persist-shim/index.js
@@ -2,7 +2,9 @@ module.exports = {
   afterInstall: function () {
     return this.addPackagesToProject([
       {name: 'redux-persist', target: '4.6.0'},
-      {name: 'babel-preset-stage-2', target: '6.24.1'}
+      {name: 'babel-preset-stage-2', target: '6.24.1'},
+      {name: 'ember-json-stringify-safe-shim', target: '0.0.3'},
+      {name: 'ember-lodash-es-shim', target: '0.0.3'}
     ])
   },
 
