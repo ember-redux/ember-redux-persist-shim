82a1696e05e1eac82c17c1e89399febdda050df5
toran billups <toranb@gmail.com>
2017-04-18 07:24:36 -0500

    fix for global.setimmediate

--- README.md
+++ README.md
@@ -2,21 +2,9 @@
 
 babel 6 is required so please use ember-cli v2.13.0+
 
+    ember install ember-redux
     ember install ember-lodash-es-shim
     ember install ember-json-stringify-safe-shim
     ember install ember-redux-persist-shim
 
 remove the ember-lodash-shim dependency from your package.json
-
-In your app/index.html be sure to hack in 2 globals :(
-
-    <script>
-      var process = {
-        env: {
-          NODE_ENV: 'development'
-        }
-      };
-      var global = {
-        setImmediate: window.setTimeout
-      }
-    </script>
--- index.js
+++ index.js
@@ -4,12 +4,25 @@
 const mergeTrees = require('broccoli-merge-trees');
 const path = require('path');
 const esTranspiler = require('broccoli-babel-transpiler');
+const replace = require('broccoli-string-replace');
 
 module.exports = {
   name: 'redux-persist',
   treeForAddon (tree) {
     const persistPath = path.dirname(require.resolve('redux-persist/src/index.js'));
-    var persistTree = esTranspiler(persistPath, {
+
+    const denodifiedPersist = replace(persistPath, {
+      files: [ '**/*.js' ],
+      patterns: [{
+        match: /process\.env\.NODE_ENV/g,
+        replacement: JSON.stringify(process.env.EMBER_ENV)
+      }, {
+        match: /global\.setImmediate/g,
+        replacement: '(fn) => setTimeout(fn, 0)'
+      }]
+    });
+
+    const persistTree = esTranspiler(denodifiedPersist, {
       presets: [
           ['stage-2']
       ]
